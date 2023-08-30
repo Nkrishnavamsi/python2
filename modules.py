a319bdde38c3b0191b3e0c403c48151ab835e9fe
Nkrishnavamsi <vamsinaidun9@gmail.com>
2023-08-30 11:52:34 +0530

    first

---
+++ modules.py
@@ -0,0 +1,20 @@
+'''
+
+from random import *
+print(randrange(1,100,10))
+'''
+
+
+#import math
+from math import * 
+
+print(pi)
+print(ceil(9.8))
+print(floor(9.8))
+
+""" 
+print(math.pi)
+print(math.ceil(9.8))  # 10
+print(math.floor(9.8)) # 9
+print(math.sqrt(100))  #10.0
+print(math.tan(1))   # """
---
+++ randommath.py
@@ -0,0 +1,14 @@
+'''
+#import math
+from math import * 
+
+print(pi)
+print(ceil(8))
+print(floor(9.8))
+'''
+from random import *
+ename=["vamsi" "vayun" "raghuram" "junaid"]
+#print random element list
+
+for x in range (100):
+    print (choice(ename))
